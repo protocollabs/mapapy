b39ace74c26390d9cf23a6bec5fec4120290d708
Hagen Paul Pfeifer <hagen@jauu.net>
2017-12-17 14:55:55 +0100

    implement info request/reply handling
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- mapapy.py
+++ mapapy.py
@@ -4,15 +4,27 @@ import socket
 import sys
 import argparse
 import struct
+import uuid
+import datetime
+import json
 
 
 PORT = 64321
 
 PROTOCOL_NULL_REQUEST_CODE = 1
 PROTOCOL_NULL_REPLY_CODE   = 2
+PROTOCOL_INFO_REQUEST_CODE = 3
+PROTOCOL_INFO_REPLY_CODE   = 4
 
 
 NULL_MSG_SIZE = 512
+
+def maparo_date():
+    dt = datetime.datetime.utcnow()
+    return dt.strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+def maparo_date_parse(string):
+    return datetime.datetime.strptime(string, '%Y-%m-%dT%H:%M:%S.%f')
 
 def init_socket(ctx):
     sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,6 +37,25 @@ def srv_msg_null_request(ctx, data, address):
     b = struct.pack('>I', PROTOCOL_NULL_REPLY_CODE) + data[4:]
     ctx['sk'].sendto(b, address)
 
+def srv_msg_info_request(ctx, data, address):
+    json_len = struct.unpack('>I', data[4:8])[0]
+    if len(data) - (4 + 4) < json_len:
+        raise Exception('packet is smaller as expected')
+    json_bytes = data[8:json_len + 8]
+    request_data = json.loads(json_bytes.decode())
+    reply_data = dict()
+    reply_data['id'] = "{}={}".format(socket.gethostname(), uuid.uuid4())
+    reply_data['seq-rp'] = request_data['seq']
+    reply_data['ts-rp'] = request_data['ts']
+    reply_data['ts'] = maparo_date()
+    reply_data['arch'] = 'unknown'
+    reply_data['os'] = 'unknown'
+    json_bytes = str.encode(json.dumps(reply_data))
+    b = struct.pack('>II', PROTOCOL_INFO_REPLY_CODE, len(json_bytes))
+    buf = b + json_bytes
+    ctx['sk'].sendto(buf, address)
+
+
 def server_recv_loop(ctx):
     while True:
         data, address = ctx['sk'].recvfrom(4096)
@@ -32,8 +63,10 @@ def server_recv_loop(ctx):
             print('message to short, should header and at least one byte')
             continue
         code = struct.unpack('>I', data[0:4])[0]
-        if code == 1:
+        if code == PROTOCOL_NULL_REQUEST_CODE:
             srv_msg_null_request(ctx, data, address)
+        elif code == PROTOCOL_INFO_REQUEST_CODE:
+            srv_msg_info_request(ctx, data, address)
         else:
             print('unhandled message code: {}'.format(code))
             continue
@@ -68,6 +101,11 @@ def client_process_null_reply(ctx, data, address):
     else:
         raise Exception('unexpected reply from client!')
 
+def timedelta_ms(timedelta):
+    return timedelta.total_seconds() * 1000.0
+
+def human_timedelta(timedelta):
+    return '{} ms'.format(timedelta_ms(timedelta))
 
 def client_process_null_message(ctx):
     # tx phase
@@ -78,12 +116,50 @@ def client_process_null_message(ctx):
     data, address = ctx['sk'].recvfrom(4096)
     return client_process_null_reply(ctx, data, address)
 
+def client_process_info_reply(ctx, data, address):
+    if len(data) <= 8:
+        raise Exception('message to short, should header and at least one byte')
+    code, length = struct.unpack('>II', data[0:8])
+    if code != PROTOCOL_INFO_REPLY_CODE:
+        print('receive invlid info reply message')
+        return False
+    assert len(data) == length + 8
+    json_bytes = data[8:length + 8]
+    reply_data = json.loads(json_bytes.decode())
+    request_date = maparo_date_parse(reply_data['ts-rp'])
+    now = datetime.datetime.utcnow()
+    delta = now - request_date
+    print("rtt: {}".format(human_timedelta(delta)))
+    return True
+
+def msg_create_info_msg(ctx):
+    msg = dict()
+    msg['id'] = "{}={}".format(socket.gethostname(), uuid.uuid4())
+    msg['seq'] = 0
+    msg['ts'] = maparo_date()
+    json_bytes = str.encode(json.dumps(msg))
+    b = struct.pack('>II', PROTOCOL_INFO_REQUEST_CODE, len(json_bytes))
+    return b + json_bytes
+
+def client_process_info_message(ctx):
+    # tx phase
+    msg = msg_create_info_msg(ctx)
+    print('send INFO message')
+    ctx['sk'].sendto(msg, (ctx['args'].addr, PORT))
+    # rx phase
+    data, address = ctx['sk'].recvfrom(4096)
+    return client_process_info_reply(ctx, data, address)
+
 
 def mode_client(ctx):
     ctx['sk'] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ok = client_process_null_message(ctx)
     if not ok:
         print("failed to process null message")
+        return
+    ok = client_process_info_message(ctx)
+    if not ok:
+        print("failed to process info message")
         return
 
 def main():
