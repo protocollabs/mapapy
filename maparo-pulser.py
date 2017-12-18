78bb213584c86152bcaef3950a016e8c261647a0
Hagen Paul Pfeifer <hagen@jauu.net>
2017-12-18 22:08:35 +0100

    start implementing start-request, start-reply
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- maparo-pulser.py
+++ maparo-pulser.py
@@ -7,17 +7,23 @@ import struct
 import uuid
 import datetime
 import json
+import pprint
 
 
 PORT = 64321
 
-PROTOCOL_NULL_REQUEST_CODE = 1
-PROTOCOL_NULL_REPLY_CODE   = 2
-PROTOCOL_INFO_REQUEST_CODE = 3
-PROTOCOL_INFO_REPLY_CODE   = 4
+PROTOCOL_NULL_REQUEST_CODE  = 1
+PROTOCOL_NULL_REPLY_CODE    = 2
+PROTOCOL_INFO_REQUEST_CODE  = 3
+PROTOCOL_INFO_REPLY_CODE    = 4
+PROTOCOL_START_REQUEST_CODE = 5
+PROTOCOL_START_REPLY_CODE   = 6
 
 
 NULL_MSG_SIZE = 512
+
+def maparo_id():
+    return "{}={}".format(socket.gethostname(), uuid.uuid4())
 
 def maparo_date():
     dt = datetime.datetime.utcnow()
@@ -46,15 +52,27 @@ def srv_msg_info_request(ctx, data, address):
     json_bytes = data[8:json_len + 8]
     request_data = json.loads(json_bytes.decode())
     reply_data = dict()
-    reply_data['id'] = "{}={}".format(socket.gethostname(), uuid.uuid4())
+    reply_data['id'] = maparo_id()
     reply_data['seq-rp'] = request_data['seq']
     reply_data['ts-rp'] = request_data['ts']
     reply_data['ts'] = maparo_date()
     reply_data['arch'] = 'unknown'
     reply_data['os'] = 'unknown'
-    reply_data['modules'] = { 'udp-pulser' : {} }
+    reply_data['modules'] = { '_udp-pulser' : {} }
     json_bytes = str.encode(json.dumps(reply_data))
     b = struct.pack('>II', PROTOCOL_INFO_REPLY_CODE, len(json_bytes))
+    buf = b + json_bytes
+    ctx['sk'].sendto(buf, address)
+
+def srv_msg_start_request(ctx, data, address):
+    json_len = struct.unpack('>I', data[4:8])[0]
+    if len(data) - (4 + 4) < json_len:
+        raise Exception('packet is smaller as expected')
+    json_bytes = data[8:json_len + 8]
+    request_data = json.loads(json_bytes.decode())
+    reply_data = dict()
+    json_bytes = str.encode(json.dumps(reply_data))
+    b = struct.pack('>II', PROTOCOL_START_REPLY_CODE, len(json_bytes))
     buf = b + json_bytes
     ctx['sk'].sendto(buf, address)
 
@@ -70,6 +88,8 @@ def server_recv_loop(ctx):
             srv_msg_null_request(ctx, data, address)
         elif code == PROTOCOL_INFO_REQUEST_CODE:
             srv_msg_info_request(ctx, data, address)
+        elif code == PROTOCOL_START_REQUEST_CODE:
+            srv_msg_start_request(ctx, data, address)
         else:
             print('unhandled message code: {}'.format(code))
             continue
@@ -77,14 +97,27 @@ def server_recv_loop(ctx):
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--configuration", help="configuration", type=str, default=None)
     parser.add_argument("--daemon", action='store_true', default=False)
     parser.add_argument('--addr', action="store",  type=str, default="localhost")
     parser.add_argument("-v", "--verbose", action="count", default=0,
                         help="increase output verbosity")
     return parser.parse_args()
 
+def load_configuration_file(args):
+    if not args.configuration:
+        print("client mode but no --configuration <file> given ...")
+        sys.exit(0)
+    config = dict()
+    exec(open(args.configuration).read(), None, config)
+    return config
+
 def init_ctx():
-    return { 'args' : parse_args() }
+    args = parse_args()
+    conf = None
+    if not args.daemon:
+        conf = load_configuration_file(args)
+    return { 'args' : args, 'conf' : conf }
 
 def mode_server(ctx):
     ctx['sk'] = init_socket(ctx)
@@ -98,7 +131,7 @@ def client_process_null_reply(ctx, data, address):
     if len(data) <= 4:
         raise Exception('message to short, should header and at least one byte')
     code = struct.unpack('>I', data[0:4])[0]
-    if code == 2 and len(data) == NULL_MSG_SIZE:
+    if code == PROTOCOL_NULL_REPLY_CODE and len(data) == NULL_MSG_SIZE:
         print('receive valid null-reply message')
         return True
     else:
@@ -134,6 +167,10 @@ def check_server_info(ctx, reply_data):
     print("arch:        {}".format(reply_data['arch']))
     print("os:          {}".format(reply_data['os']))
     print("modules supported: {}".format(",".join(reply_data['modules'].keys())))
+    if not '_udp-pulser' in reply_data['modules']:
+        print("udp pulser module not suported by server")
+        return False
+    return True
 
 def client_process_info_reply(ctx, data, address, rx_time):
     if len(data) <= 8:
@@ -147,12 +184,11 @@ def client_process_info_reply(ctx, data, address, rx_time):
     json_bytes = data[8:length + 8]
     reply_data = json.loads(json_bytes.decode())
     check_time_sync(ctx, reply_data, rx_time)
-    check_server_info(ctx, reply_data)
-    return True
+    return check_server_info(ctx, reply_data)
 
 def msg_create_info_msg(ctx):
     msg = dict()
-    msg['id'] = "{}={}".format(socket.gethostname(), uuid.uuid4())
+    msg['id'] = maparo_id()
     msg['seq'] = 0
     msg['ts'] = maparo_date()
     json_bytes = str.encode(json.dumps(msg))
@@ -169,6 +205,39 @@ def client_process_info_message(ctx):
     rx_time = datetime.datetime.utcnow()
     return client_process_info_reply(ctx, data, address, rx_time)
 
+def msg_create_start_request(ctx):
+    msg = dict()
+    msg['id'] = maparo_id()
+    msg['seq'] = 0
+    msg['module'] = dict()
+    msg['module']['name'] = '_udp_pulser'
+    msg['module']['mode'] = 'server'
+    msg['module']['configuration'] = ctx['conf']
+    pprint.pprint(msg)
+    json_bytes = str.encode(json.dumps(msg))
+    b = struct.pack('>II', PROTOCOL_START_REQUEST_CODE, len(json_bytes))
+    return b + json_bytes
+
+def client_process_start_reply(ctx, data, address):
+    if len(data) <= 8:
+        raise Exception('message to short, should header and at least one byte')
+    code, length = struct.unpack('>II', data[0:8])
+    if code != PROTOCOL_START_REPLY_CODE:
+        print('receive invalid start reply message')
+        return False
+    assert len(data) == length + 8
+    print('receive valid start-reply message')
+    json_bytes = data[8:length + 8]
+    reply_data = json.loads(json_bytes.decode())
+    #check_time_sync(ctx, reply_data, rx_time)
+    #return check_server_info(ctx, reply_data)
+
+def client_process_start_message(ctx):
+    msg = msg_create_start_request(ctx)
+    print('send start-request message')
+    ctx['sk'].sendto(msg, (ctx['args'].addr, PORT))
+    data, address = ctx['sk'].recvfrom(4096)
+    return client_process_start_reply(ctx, data, address)
 
 def mode_client(ctx):
     ctx['sk'] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -179,6 +248,10 @@ def mode_client(ctx):
     ok = client_process_info_message(ctx)
     if not ok:
         print("failed to process info message")
+        return
+    ok = client_process_start_message(ctx)
+    if not ok:
+        print("failed to process start message")
         return
 
 def main():
