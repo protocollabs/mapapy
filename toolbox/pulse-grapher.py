552742febbbaa0a0c1952ca9820a271e6f3b1ee0
Hagen Paul Pfeifer <hagen@jauu.net>
2018-01-18 20:52:18 +0100

    more stats: no packets, duration, min, max, ...
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- toolbox/pulse-grapher.py
+++ toolbox/pulse-grapher.py
@@ -70,6 +70,44 @@ def correlate_data(data):
 
 def data_stats(data):
     stats = dict()
+    stats['time-min'] = datetime.datetime(5000, 1, 1, 13, 37)
+    stats['time-max'] = datetime.datetime(   1, 1, 1, 13, 37)
+    stats['duration-min'] = datetime.timedelta(1000, 0, 0)
+    stats['duration-max'] = datetime.timedelta(0, 0, 0)
+    stats['tx-packets'] = 0
+    stats['rx-packets'] = 0
+    for stream_id, stream_data in data.items():
+        for seq_no in sorted(stream_data):
+            entry = stream_data[seq_no]
+            assert(entry['client']) # failure if received but not transmitted
+            stats['tx-packets'] += 1
+            if not entry['server']:
+                print("packet loss")
+                continue
+            stats['rx-packets'] += 1
+            stats['time-min'] = min(stats['time-min'], entry['server']['time'])
+            stats['time-min'] = min(stats['time-min'], entry['client']['time'])
+
+            stats['time-max'] = max(stats['time-max'], entry['server']['time'])
+            stats['time-max'] = max(stats['time-max'], entry['client']['time'])
+
+            duration = entry['server']['time'] - entry['client']['time']
+            duration_sec = duration.total_seconds()
+            if duration_sec < 0:
+                print("packet was received before it was transmited")
+                print(" stream: {}, sequence number: {}".format(stream_id, seq_no))
+            stats['duration-min'] = min(stats['duration-min'], duration)
+            stats['duration-max'] = max(stats['duration-max'], duration)
+
+    print("Measurement Duration: {} seconds".format( (stats['time-max'] - stats['time-min']).total_seconds()))
+    print("Transmission Duration Min: {} ms".format(stats['duration-min'].total_seconds() * 1000.0))
+    print("Transmission Duration Max: {} ms".format(stats['duration-max'].total_seconds() * 1000.0))
+    print("Date Min: {}".format(stats['time-min']))
+    print("Date Max: {}".format(stats['time-max']))
+    print("Packets Transmitted: {}".format(stats['tx-packets']))
+    print("Packets Received: {}".format(stats['rx-packets']))
+
+
 
 def normalize(data, raw_data):
     """ normalize time by adding (possible negative) recorded delta to server time"""
@@ -85,12 +123,12 @@ def normalize(data, raw_data):
 
 def print_correlated(correlated):
     for stream_id, stream_data in correlated.items():
-        print('Stream: {}'.format(stream_id))
+        #print('Stream: {}'.format(stream_id))
         for seq_no in sorted(stream_data):
-            print('  seq: {}'.format(seq_no))
+            #print('  seq: {}'.format(seq_no))
             entry = stream_data[seq_no]
             diff = entry['server']['time'] - entry['client']['time']
-            print("    client -> [{:.3f} ms] -> server".format(diff.total_seconds() * 1000.0))
+            #print("    client -> [{:.3f} ms] -> server".format(diff.total_seconds() * 1000.0))
 
 def process(data):
     check_data(data)
